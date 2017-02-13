0079b841451949145e52b57e1ed8b10c1dacf01b
Michael Ieti <michael.ieti@genesisenergy.co.nz>
2017-02-14 12:10:45 +1300

    - street view icon, + color scale

--- js/mapdash.js
+++ js/mapdash.js
@@ -12,7 +12,26 @@ var pc;
 // called on completing google maps API. The launch point for any other function.
 function main() {
     colors = [
-'#6dfdcf', '#6cfbc8', '#6cf9c1', '#6bf7ba', '#6af5b3', '#68f3ac', '#67f1a5', '#66ee9e', '#64ec97', '#62ea90', '#60e889', '#5ee682', '#5ce47b', '#5ae273', '#58e06c', '#55de65', '#52dc5d', '#4fda55', '#4cd84d', '#49d645', '#45d43c', '#41d232', '#3dd027', '#38ce19', '#33cc00', '#42ce00', '#4fd100', '#5ad300', '#65d500', '#6ed700', '#77da00', '#80dc00', '#88de00', '#91e000', '#99e200', '#a1e500', '#a8e700', '#b0e900', '#b7eb00', '#bfed00', '#c6ef00', '#cdf100', '#d4f300', '#dcf500', '#e3f700', '#eaf900', '#f1fb00', '#f8fd00', '#ffff00', '#fffb00', '#fff700', '#fff300', '#ffee00', '#ffea00', '#ffe600', '#ffe200', '#ffde00', '#ffda00', '#ffd500', '#ffd100', '#ffcd00', '#ffc900', '#ffc500', '#ffc000', '#ffbc00', '#ffb800', '#ffb300', '#ffaf00', '#ffab00', '#ffa600', '#ffa200', '#ff9d00', '#ff9900', '#ff9600', '#ff9200', '#ff8f00', '#ff8b00', '#ff8800', '#ff8400', '#ff8000', '#ff7d00', '#ff7900', '#ff7500', '#ff7100', '#ff6d00', '#ff6900', '#ff6500', '#ff6100', '#ff5c00', '#ff5700', '#ff5300', '#ff4d00', '#ff4800', '#ff4200', '#ff3c00', '#ff3500', '#ff2d00', '#ff2400', '#ff1700', '#ff0000'
+        '#6dfdcf', '#6cfbc8', '#6cf9c1', '#6bf7ba', '#6af5b3',
+        '#68f3ac', '#67f1a5', '#66ee9e', '#64ec97', '#62ea90',
+        '#60e889', '#5ee682', '#5ce47b', '#5ae273', '#58e06c',
+        '#55de65', '#52dc5d', '#4fda55', '#4cd84d', '#49d645',
+        '#45d43c', '#41d232', '#3dd027', '#38ce19', '#33cc00',
+        '#42ce00', '#4fd100', '#5ad300', '#65d500', '#6ed700',
+        '#77da00', '#80dc00', '#88de00', '#91e000', '#99e200',
+        '#a1e500', '#a8e700', '#b0e900', '#b7eb00', '#bfed00',
+        '#c6ef00', '#cdf100', '#d4f300', '#dcf500', '#e3f700',
+        '#eaf900', '#f1fb00', '#f8fd00', '#ffff00', '#fffb00',
+        '#fff700', '#fff300', '#ffee00', '#ffea00', '#ffe600',
+        '#ffe200', '#ffde00', '#ffda00', '#ffd500', '#ffd100',
+        '#ffcd00', '#ffc900', '#ffc500', '#ffc000', '#ffbc00',
+        '#ffb800', '#ffb300', '#ffaf00', '#ffab00', '#ffa600',
+        '#ffa200', '#ff9d00', '#ff9900', '#ff9600', '#ff9200',
+        '#ff8f00', '#ff8b00', '#ff8800', '#ff8400', '#ff8000',
+        '#ff7d00', '#ff7900', '#ff7500', '#ff7100', '#ff6d00',
+        '#ff6900', '#ff6500', '#ff6100', '#ff5c00', '#ff5700',
+        '#ff5300', '#ff4d00', '#ff4800', '#ff4200', '#ff3c00',
+        '#ff3500', '#ff2d00', '#ff2400', '#ff1700', '#ff0000'
     ];
     schools = [
         {
@@ -615,7 +634,8 @@ function main() {
     };
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
-        center: centerNZ
+        center: centerNZ,
+        streetViewControl: false,
     });
 
     // TODO: set up a matrix div
